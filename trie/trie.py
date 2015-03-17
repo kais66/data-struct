42d060e7568c59ab276d41743554107ccf777d81
Kai Su <kais@winlab.rutgers.edu>
2015-03-17 00:07:43 -0400

    trie

---
+++ trie/trie.py
@@ -0,0 +1,42 @@
+# http://www.geeksforgeeks.org/trie-insert-and-search/
+class TrieNode:
+    def __init__(self):
+        self.isEnd = False
+        self.child = [None for i in range(256)] # Used to hold TrieNode pointers
+
+    def add(self, s):
+        nd = self
+        for i in range(len(s)):
+            ind = ord(s[i])
+            if not nd.child[ind]:
+                nd.child[ind] = TrieNode()
+                #print 'child isEnd: %s, i= %d' % (str(self.child[ind].isEnd), i) 
+            nd = nd.child[ind]
+        nd.isEnd = True
+
+    def search(self, s):
+        nd = self
+        for i in range(len(s)):
+            ind = ord(s[i])
+            if not nd.child[ind]: return False
+            nd = nd.child[ind]
+            #print 'nd isEnd: %s, i= %d' % (str(nd.isEnd), i) 
+        return nd.isEnd
+
+if __name__ == '__main__':
+    trie = TrieNode()
+    for w in ['abc', 'acd', 'bzz', 'aa' ]:
+        trie.add(w)
+    print trie.search('abc')
+    print trie.search('acd')
+    print trie.search('bzz')
+    print trie.search('aa')
+
+    print trie.search('xzd')
+    print trie.search('bc')
+    print trie.search('bz')
+    print trie.search('b')
+ 
+
+
+
